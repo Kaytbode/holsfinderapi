06b1d6a50c387f7e64ee1fe6cded59f5e843094e
kaytbode <bkaoluborode@gmail.com>
2019-09-17 17:17:34 +0100

    fix: cors issues

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import routes from './routes/index';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
